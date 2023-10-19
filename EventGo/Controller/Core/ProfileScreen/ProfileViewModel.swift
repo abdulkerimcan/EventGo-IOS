2dafe0d18e7a144fc7eda8dacc3f10274fe588a0
abdulkerimcan <abdulkerimcann@outlook.com>
2023-10-20 00:54:37 +0300

    completed ui

---
+++ EventGo/Controller/Core/ProfileScreen/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  EventGo
+//
+//  Created by Abdulkerim Can on 19.10.2023.
+//
+
+import Foundation
---
+++ EventGo/Utilities/Extensions/UIImageView+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIImageView+Ext.swift
+//  EventGo
+//
+//  Created by Abdulkerim Can on 20.10.2023.
+//
+
+import Foundation
